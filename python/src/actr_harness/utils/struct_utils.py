76a063f270ad2890490b2904ccfc70110ddb5222
Chshine1 <chshine@qq.com>
2026-07-28 23:35:49 +0800

    fix: fix struct utils

--- python/src/actr_harness/utils/struct_utils.py
+++ python/src/actr_harness/utils/struct_utils.py
@@ -1,17 +1,24 @@
 from betterproto.lib.google.protobuf import Struct, Value
+from betterproto.lib.std.google.protobuf import ListValue
+
+
+def _to_value(val):
+    if isinstance(val, dict):
+        return Value(struct_value=dict_to_struct(val))
+    elif isinstance(val, list):
+        return Value(list_value=ListValue(values=[_to_value(v) for v in val]))
+    elif isinstance(val, str):
+        return Value(string_value=val)
+    elif isinstance(val, bool):
+        return Value(bool_value=val)
+    elif isinstance(val, (int, float)):
+        return Value(number_value=val)
+    else:
+        raise TypeError(f"Unsupported type for Value: {type(val)}")
 
 
 def dict_to_struct(data: dict) -> Struct:
     s = Struct()
     for key, value in data.items():
-        if isinstance(value, dict):
-            s.fields[key] = Value(struct_value=dict_to_struct(value))
-        elif isinstance(value, str):
-            s.fields[key] = Value(string_value=value)
-        elif isinstance(value, (int, float)):
-            s.fields[key] = Value(number_value=value)
-        elif isinstance(value, bool):
-            s.fields[key] = Value(bool_value=value)
-        else:
-            raise TypeError(f"Unsupported type for Struct: {type(value)}")
+        s.fields[key] = _to_value(value)
     return s
